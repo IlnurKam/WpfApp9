03555e83fb447046b2d9b4acd302f4dee90274b7
IlnurKam <kamal673@yandex.ru>
2022-03-25 19:39:53 +0300

    Добавьте файлы проекта.

---
+++ WpfApp9/Models/Ariph.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp9.Models
+{
+    static class Ariph
+    {
+        public static int Add(int a, int b) => a + b;
+        public static double CircumferenceLength(double r) => 2 * Math.PI * r;
+    }
+}
---
+++ WpfApp9/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using WpfApp9.Models;
+
+namespace WpfApp9.ViewModels
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private double number1;
+        public double Number1
+        {
+            get => number1;
+            set
+            {
+                number1 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double number3;
+        public double Number3
+        {
+            get => number3;
+            set
+            {
+                number3 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand CircumferenceLengthCommand { get; }
+
+        private void OnCircumferenceLengthCommandExecute(object p)
+        {
+            Number3 = Ariph.CircumferenceLength(Number1);
+        }
+
+        private bool CanCircumferenceLengthCommandExecuted(object p)
+        {
+            //if (Number1 != 0)
+            //    return true;
+            //else
+            //    return false;
+            return true;
+        }
+
+        public MainWindowViewModel()
+        {
+            CircumferenceLengthCommand = new RelayCommand(OnCircumferenceLengthCommandExecute, CanCircumferenceLengthCommandExecuted);
+        }
+    }
+}
